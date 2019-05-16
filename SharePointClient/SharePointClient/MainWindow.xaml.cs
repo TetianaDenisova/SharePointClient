c43d5955eff467c89c493e6773c29d70becc499d
tetiana.kozlovska <t.kozlovska@abtollc.com>
2019-05-16 15:26:51 +0300

    adding basic operations

--- SharePointClient/SharePointClient/MainWindow.xaml.cs
+++ SharePointClient/SharePointClient/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.SharePoint.Client;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SharePointClient
 {
@@ -20,6 +8,8 @@ namespace SharePointClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ClientContext context;
+        private Web web;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +17,53 @@ namespace SharePointClient
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Login();
+            GetTitle();
+            var myList = "My List";
 
+        }
+
+        private void CreateList(string listName)
+        {
+            ListCreationInformation creationInfo = new ListCreationInformation();
+            creationInfo.Title = listName;
+            creationInfo.TemplateType = (int)ListTemplateType.Announcements;
+            List list = web.Lists.Add(creationInfo);
+            list.Description = "New Description";
+
+            list.Update();
+            context.ExecuteQuery();
+        }
+        private void DeleteList(string listName)
+        {
+            List list = web.Lists.GetByTitle(listName);
+            list.DeleteObject();
+
+            context.ExecuteQuery();
+        }
+        private void UpdateTitle()
+        {
+            web.Title = "HelloTania";
+            web.Update();
+            context.ExecuteQuery();
+        }
+
+        private void GetTitle()
+        {
+            context.Load(web, w => w.Title, w => w.Description, w => w.Fields);
+            context.ExecuteQuery();
+            tbFullInfo.Text = web.Title;
+        }
+
+        private void Login()
+        {
+            context = new ClientContext("https://skybow.sharepoint.com/sites/TestTeamSite");
+
+            context.Credentials = new SharePointOnlineCredentials("tetiana.kozlovska@skybow.com", pbox1.SecurePassword.Copy());
+            web = context.Web;
+            context.Load(web);
+            context.ExecuteQuery();
+            label1.Content = web.Title;
         }
     }
 }
